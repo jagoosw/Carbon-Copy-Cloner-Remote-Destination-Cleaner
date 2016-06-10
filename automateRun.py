83fdb73c33cb8e79d0914917318f06f2bb1bbde7
Jago Strong-Wright <jago@null.net>
2016-06-10 14:13:00 +0100

    Very Little

--- automateRun.py
+++ automateRun.py
@@ -1,12 +1,5 @@
-
-
-#Imports main cleaning file
 import script
-
-#Date time library for getting the time
 import datetime
-
-#Time Library for sleeping
 import time
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -15,31 +8,22 @@ locations = ["/Volumes/Backup1/_CCC SafetyNet", "/Volumes/Backup2/_CCC SafetyNet
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 
 
-
-#Infinite loop to make it run forever
+#This keeps the program running forever although I'm sure there are more efficent ways to do this
 while True:
-	#Incase it doesn't work (e.g. permission error)
 	try:
-		#Waits untill the hour is 7am, will run at anytime in the hour depending on when you start it
 		while "07:00" not in str(datetime.datetime.now()):
 			print("Waiting until 7am")
 			time.sleep(3300) 
 		
-		#Cleans each location
 		for location in locations:
 			script.prunePath(location)
 		
-		#Displays time of clean
 		print("At: ", datetime.datetime.now())
 
-	#If permission denied for folder shutil raises OSError, this statement catches it	
 	except OSError as err:
 		print("Looks like you don't have permission to modify that folder, change it with an administrators account and try again.  OS error: {0}".format(err))
-	#If user exits with ^C exits and prevents it being caught by exceptions
 	except KeyboardInterrupt as err:
 		print("Goodbye then...")
-		#Exits
 		raise SystemExit
-	#Catches other errors
 	except:
 		print("Unexpected error, oh well")
--- script.py
+++ script.py
@@ -1,45 +1,24 @@
 
-#Used to delete the folderrs
 import shutil
-
-#Used to list the content of _CCC SafetyNet
 import os
-
-#Used to find current date 
 import datetime
-
-#Used to find the length of months
 import calendar
 
-#In function so accessible from automateRun.py
 #path is the full path to _CCC SafetyNet - e.g. "/Volumes/Backup/_CCC SafetyNet"
 def prunePath(path):
-	
-	#Lists contenct of folder
+
 	pathAllFiles = os.listdir(path)
-
-	#Finds the age of folders to delete	
 	maxMonth = str(datetime.datetime.now().month - 2)
-	#In number version to find length of month
 	maxMonthInt = datetime.datetime.now().month - 2
-	#When finding files to delete used so that files with the max month in the time are not also deleted (e.g. file could be called 2016-05-21 ... 21-04-36 which would be found in an april search due to the time of the backup (21-04-36))
 	currentYear = str(datetime.datetime.now().year)
-
-	#Initiate list of 
 	deleteFoulders = list()
 
 	if len(maxMonth) < 2:
-		#Formats the month so it is as the folder is
 		maxMonth = "0" + maxMonth
 		
-	#Cycles through the safety net folder looking for 2 month old olders	
 	for folder in pathAllFiles:
-		#Check if the folder is the right age
 		if currentYear+"-"+maxMonth+"-" in folder:
-			#Checks that its not the last one of the month
 			if currentYear+"-"+maxMonth+"-"+str(calendar.monthrange(datetime.datetime.now().year, maxMonthInt)[1]) not in folder:
-				#Deletes the folder
 				shutil.rmtree(path+"/"+folder)
 
-	#Tells you its finished for that run			
 	print("Complete clean for path: ", path)
